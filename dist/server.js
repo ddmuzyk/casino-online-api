d12b2ca3c786efdce99c8a9e79c474cf5c40bada
ddmuzyk <domino2k1@gmail.com>
2024-01-01 16:25:11 +0100

    changed starting commands

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import * as PokerEvaluator from 'poker-evaluator';
 import cors from "cors";
-// PokerEvaluator.evalHand(['As', 'Ks', 'Qs', 'Js', 'Ts', '3c', '7d']);
-// Evaluate a different hand
-PokerEvaluator.evalHand(['Js', '3h', '2d', 'Tc', 'Qh', '8s', '5d']);
 const app = express();
 app.use(express.json());
 app.use(cors());
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,6 @@ import { Request, Response } from 'express';
 import * as PokerEvaluator from 'poker-evaluator';
 import cors from "cors";
 
-// PokerEvaluator.evalHand(['As', 'Ks', 'Qs', 'Js', 'Ts', '3c', '7d']);
-// Evaluate a different hand
-PokerEvaluator.evalHand(['Js', '3h', '2d', 'Tc', 'Qh', '8s', '5d'])
-
-
 const app = express();
 app.use(express.json());
 app.use(cors())
