1e762ecec9d9f10fb80b6eca5cbb23f2644839ae
ddmuzyk <domino2k1@gmail.com>
2024-02-15 21:21:44 +0100

    setted up the database connection

---
+++ dist/controllers/register.js
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import pg from 'pg';
+const { Client } = pg;
+const client = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
+await client.connect();
+client.query('SELECT * FROM users', (err, res) => {
+    if (!err) {
+        console.log(res.rows);
+    }
+    client.end();
+});
+//# sourceMappingURL=register.js.map
